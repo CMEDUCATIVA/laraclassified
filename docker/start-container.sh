b38c84661186a14fd58305d3e13be8fc1a31e53d
CMEDUCATIVA <161378183+CMEDUCATIVA@users.noreply.github.com>
2025-11-24 12:48:51 -0500

    Update start-container.sh

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -14,8 +14,12 @@ for dir in /var/www/html/config /var/www/html/public /var/www/html/lang; do
     if [ -d "$dir" ]; then
         chown -R www-data:www-data "$dir"
         chmod -R 775 "$dir"
+        find "$dir" -type f -exec chmod 664 {} \;
     fi
 done
+
+php artisan config:clear --ansi || true
+php artisan cache:clear --ansi || true
 
 php-fpm --daemonize
 
