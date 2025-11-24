4f2fb17c6dd5dec8c0acf7720dc109393b800468
CMEDUCATIVA <161378183+CMEDUCATIVA@users.noreply.github.com>
2025-11-24 12:23:38 -0500

    Update start-container.sh

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -10,6 +10,13 @@ if [ ! -L /var/www/html/public/storage ]; then
     php artisan storage:link --ansi || true
 fi
 
+for dir in /var/www/html/config /var/www/html/public /var/www/html/lang; do
+    if [ -d "$dir" ]; then
+        chown -R www-data:www-data "$dir"
+        chmod -R 775 "$dir"
+    fi
+done
+
 php-fpm --daemonize
 
 exec nginx -g "daemon off;"
